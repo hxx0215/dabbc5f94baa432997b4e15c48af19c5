0533ca3ac6f2654fd5e5c95fa9f1da9c3a1d3375
hxx0215 <huangxinxing@kdanmobile.com>
2014-09-17 21:22:27 +0800

    装修管理 - 新增HNDecorateControlViewController

---
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNDecorateControlViewController.m
@@ -0,0 +1,37 @@
+//
+//  HNDecorateControlViewController.m
+//  edecorate
+//
+//  Created by hxx on 9/17/14.
+//
+//
+
+#import "HNDecorateControlViewController.h"
+
+@interface HNDecorateControlViewController ()
+
+@end
+
+@implementation HNDecorateControlViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
