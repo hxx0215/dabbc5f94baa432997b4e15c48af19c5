d81b64823489da4e91e7e1041601b73024189e5e
liuxh532 <786995209@qq.com>
2014-09-18 21:37:09 +0800

    装修管控 － 临时用火用电

---
+++ 工程/edecorate/edecorate/HNTemporaryTableViewCell.m
@@ -0,0 +1,50 @@
+//
+//  HNTemporaryTableViewCell.m
+//  edecorate
+//
+//  Created by 刘向宏 on 14-9-18.
+//
+//
+
+#import "HNTemporaryTableViewCell.h"
+
+@interface HNTemporaryTableViewCell ()
+@property (nonatomic, strong)UILabel *roomLabel;
+@property (nonatomic, strong)UILabel *statusLabel;
+@end
+@implementation HNTemporaryTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+        self.roomLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width * 0.8, 30)];
+        self.roomLabel.numberOfLines = 2;
+        self.roomLabel.font = [UIFont systemFontOfSize:15];
+        self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.roomLabel.frame) + 8, self.bounds.size.width * 0.8, 35)];
+        self.statusLabel.font = [UIFont systemFontOfSize:28];
+        [self.contentView addSubview:self.roomLabel];
+        [self.contentView addSubview:self.statusLabel];
+        
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    return self;
+}
+
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+- (void)setRoomName:(NSString *)roomname{
+    self.roomLabel.text = roomname;
+}
+- (void)setStatus:(NSString *)status{
+    self.statusLabel.text = status;
+}
+@end
+
