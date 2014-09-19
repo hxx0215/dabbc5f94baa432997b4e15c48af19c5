f45b815f587725352f869c02de05ca096d8fdb92
qtongtek_xiongbin <xiongbin@qtongtek.com>
2014-09-19 08:33:40 -0700

    add new pages of officePasses Signed-off-by: qtongtek_xiongbin <xiongbin@qtongtek.com>

---
+++ 工程/edecorate/edecorate/HNOfficePassesApplyViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNOfficePassesApplyViewController.h
+//  edecorate
+//
+//  Created by 熊彬 on 14-9-19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNOfficePassesApplyViewController : UIViewController
+
+@end
---
+++ 工程/edecorate/edecorate/HNOfficePassesDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNOfficePassesDetailsViewController.h
+//  edecorate
+//
+//  Created by 熊彬 on 14-9-19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNOfficePassesDetailsViewController : UIViewController
+
+@end
