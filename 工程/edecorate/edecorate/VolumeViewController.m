fa16625b2d27c04c879ba72f8f21138f43ac1af6
liuxh532 <786995209@qq.com>
2014-09-27 19:38:44 +0800

    商家后台管理－ 消费卷验证

--- 工程/edecorate/edecorate/VolumeViewController.m
+++ 工程/edecorate/edecorate/VolumeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "VolumeViewController.h"
+#include "UIView+AHKit.h"
 
 @interface VolumeViewController ()<UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet UISegmentedControl *viewSegmentedControl;
@@ -22,22 +23,86 @@
 
 @implementation VolumeViewController
 
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self.viewSegmentedControl setTitle:NSLocalizedString(@"Consumer verification", nil) forSegmentAtIndex:0];
+    [self.viewSegmentedControl setTitle:NSLocalizedString(@"Verify effective", nil) forSegmentAtIndex:1];
+    
+    self.volumeNOTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     self.volumeNOTextField.delegate = self;
+    self.volumePWTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     self.volumePWTextField.delegate = self;
+    self.volumePWTextField.secureTextEntry = YES;
+    [self labelWithTitle:NSLocalizedString(@"Volume No.", nil) label:self.volumeNOTitleLabel];
+    [self labelWithTitle:NSLocalizedString(@"Consumer password", nil) label:self.volumePWTitleLabel];
+    
+    self.OKButton.layer.borderWidth = 1.0;
+    self.OKButton.layer.borderColor = [UIColor blackColor].CGColor;
+    self.OKButton.width = 45;
+    [self.OKButton setTitle:NSLocalizedString(@"OK", nil) forState:UIControlStateNormal];
+    self.cancelButton.layer.borderWidth = 1.0;
+    self.cancelButton.layer.borderColor = [UIColor blackColor].CGColor;
+    self.cancelButton.width = 45;
+    [self.cancelButton setTitle:NSLocalizedString(@"Cancel", nil) forState:UIControlStateNormal];
+    
+    
+    [self.viewSegmentedControl addTarget:self action:@selector(segmentClick:)forControlEvents:UIControlEventValueChanged];
+    
+    self.volumeStatusLabel.text = nil;
+}
+
+- (void)labelWithTitle:(NSString *)title label:(UILabel*)lab
+{
+    [lab setText:title];
+    [lab sizeToFit];
+    lab.font = [UIFont systemFontOfSize:12];
+    lab.numberOfLines = 2;
+    
+    lab.layer.borderColor = [UIColor blackColor].CGColor;
 }
 
 
 - (IBAction)OKButtonClick:(id)sender
 {
+    NSInteger Index = self.viewSegmentedControl.selectedSegmentIndex;
+    switch (Index)
+    {
+        case 0:
+            self.volumeStatusLabel.text = @"验证通过";
+            break;
+        case 1:
+            self.volumeStatusLabel.text = @"此消费卷有效";
+            break;
+        default:
+            break;
+    }
 }
 
 - (IBAction)CancelButtonClick:(id)sender
 {
+    [self.navigationController popViewControllerAnimated:YES];
 }
+
+- (IBAction)segmentClick:(id)sender {
+    
+    NSInteger Index = self.viewSegmentedControl.selectedSegmentIndex;
+    switch (Index)
+    {
+        case 0:
+            self.volumePWTextField.hidden = NO;
+            self.volumePWTitleLabel.hidden = NO;
+            break;
+        case 1:
+            self.volumePWTextField.hidden = YES;
+            self.volumePWTitleLabel.hidden = YES;
+            break;
+        default:
+            break;
+    }
+    self.volumeStatusLabel.text = nil;
+}
+
 
 - (BOOL)textFieldShouldClear:(UITextField *)textField               // called when clear button pressed. return NO to ignore (no notifications)
 {
