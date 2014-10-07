66093494f36ca8d67a83a9a621914a3adb67c627
qtongtek_xiongbin <xiongbin@qtongtek.com>
2014-10-07 07:00:30 -0700

    Signed-off-by: qtongtek_xiongbin <xiongbin@qtongtek.com>

--- 工程/edecorate/edecorate/HNOfficePassesViewController.m
+++ 工程/edecorate/edecorate/HNOfficePassesViewController.m
@@ -10,6 +10,9 @@
 #import "HNOfficePassedTableViewCell.h"
 #import "HNOfficePassesDetailsViewController.h"
 #import "HNOfficePassesApplyViewController.h"
+#import "MBProgressHUD.h"
+#import "JSONKit.h"
+#import "NSString+Crypt.h"
 
 //@interface HNOfficePassModel : NSObject
 //@property (nonatomic, strong)NSString *roomName;
@@ -40,17 +43,57 @@
     [self.view addSubview:self.tTableView];
     
     self.navigationItem.title=@"办理出入证";
-    self.modelList=[[NSMutableArray alloc] init];
+    [self GetPassList:@"admin"];
+   // self.modelList=[[NSMutableArray alloc] init];
     
-    HNTemporaryModel *tModel=[[HNTemporaryModel alloc] init];
-    tModel.roomName=@"施工房号：大富大贵花园A座001";
-    tModel.status = TemporaryStatusCustom;
-    [self.modelList addObject:tModel];
-    
-    HNTemporaryModel *TModel2=[[HNTemporaryModel alloc] init];
-    TModel2.roomName=@"施工房号：大富大贵花园A座002";
-    TModel2.status=TemporaryStatusCustom;
-    [self.modelList addObject:TModel2];
+//    HNTemporaryModel *tModel=[[HNTemporaryModel alloc] init];
+//    tModel.roomName=@"施工房号：大富大贵花园A座001";
+//    tModel.status = TemporaryStatusCustom;
+//    [self.modelList addObject:tModel];
+//    
+//    HNTemporaryModel *TModel2=[[HNTemporaryModel alloc] init];
+//    TModel2.roomName=@"施工房号：大富大贵花园A座002";
+//    TModel2.status=TemporaryStatusCustom;
+//    [self.modelList addObject:TModel2];
+}
+
+- (NSDictionary *)encodeWithPassModel:(NSString *)shopid{
+    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:shopid,@"mshopid", nil];
+    return dic;
+}
+//获取出入证列表:调用接口
+-(void)GetPassList:(NSString *)myshopid{
+    MBProgressHUD *hud=[MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = NSLocalizedString(@"Loading", nil);
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    //拼传输参数(json)
+    NSString *RequestJsonStr=[[self encodeWithPassModel:myshopid] JSONString];
+    request.URL=[NSURL URLWithString:[NSString createResponseURLWithMethod:@"get.pass.list" Params:RequestJsonStr]];
+    NSString *contentType=@"text/html";
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    //开始异步调用
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        [MBProgressHUD hideHUDForView:self.view animated:YES];
+        if (data) {
+            NSString *returnStr=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            NSString *returnJson=[NSString decodeFromPercentEscapeString:[returnStr decryptWithDES]];
+            if(YES)
+            {
+                //查询成功
+                UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"测试提示" message:returnJson delegate:nil cancelButtonTitle:NSLocalizedString(@"OK",nil) otherButtonTitles:nil];
+                [alert show];
+            }else
+            {
+                //查询失败
+                UIAlertView *alert=[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Connection Error", nil) message:NSLocalizedString(@"错误1", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK",nil) otherButtonTitles:nil];
+                [alert show];
+            }
+        }else
+        {
+            UIAlertView *alert=[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Connection Error", nil) message:NSLocalizedString(@"请检查网络是否连接", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK",nil) otherButtonTitles:nil];
+            [alert show];
+        }
+    }];
 }
 
 -(void) viewWillAppear:(BOOL)animated{
