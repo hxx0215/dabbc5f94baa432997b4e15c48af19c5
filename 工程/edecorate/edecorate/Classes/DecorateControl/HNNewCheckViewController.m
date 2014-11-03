435fba6ef8ef69eb689a9b35b0049cc86bd35059
hxx0215 <hxx0215@gmail.com>
2014-11-03 22:17:09 +0800

    验收新增

--- 工程/edecorate/edecorate/Classes/DecorateControl/HNNewCheckViewController.m
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNNewCheckViewController.m
@@ -15,6 +15,8 @@
 @property (nonatomic, strong) HNDecorateChoiceView *pickView;
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *dataArr;
+@property (nonatomic, strong) UIButton *submit;
+@property (nonatomic, strong) NSString *curDeclareId;
 @end
 
 @implementation HNNewCheckViewController
@@ -28,6 +30,15 @@
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, self.pickView.bottom + 5, self.view.width, 300)];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    
+    self.submit = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.submit setTitle:@"提交" forState:UIControlStateNormal];
+    self.submit.backgroundColor = [UIColor orangeColor];
+    self.submit.width = 100;
+    self.submit.height = 60;
+    self.submit.right = self.view.right - 5;
+    [self.submit addTarget:self action:@selector(submitData:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.submit];
     [self.view addSubview:self.pickView];
     [self.view addSubview:self.tableView];
 }
@@ -36,12 +47,16 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.submit.centerY = self.tableView.bottom +(self.view.height - self.tableView.bottom) / 2;
+}
 - (void)updataDecorateInformation:(HNDecorateChoiceModel *)model{
     if (!model || [model.declareId isEqualToString:@""]){
         [self showNoData];
     }else{
         NSDictionary *sendDic = @{@"mshopid": [[HNLoginData shared] mshopid],@"declareid" : model.declareId};
+        self.curDeclareId = model.declareId;
         NSString *sendJson = [sendDic JSONString];
         NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
         request.URL = [NSURL URLWithString:[NSString createResponseURLWithMethod:@"get.acceptance.details" Params:sendJson]];
@@ -86,8 +101,19 @@
     
 //    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
 }
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 50.0;
+}
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.width, 50)];
+    view.backgroundColor = [UIColor grayColor];
+    UILabel *label = [[UILabel alloc] init];
+    label.font = [UIFont systemFontOfSize:15.0];
+    label.text = [self.dataArr[section] objectForKey:@"typename"];
+    [label sizeToFit];
+    [view addSubview:label];
+    label.left = 5;
+    label.centerY = view.height / 2;
     return view;
 }
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
@@ -106,5 +132,33 @@
     cell.type = [[self.dataArr[indexPath.section] objectForKey:@"ItemBody"][indexPath.row] objectForKey:@"bodytype"];
     cell.itemId =[self.dataArr[indexPath.section] objectForKey:@"itemId"];
     return cell;
+}
+- (void)submitData:(id)sender{
+    if ([self.dataArr count]<1)
+        return;
+    NSMutableDictionary *sendDic = [@{@"declareid": self.curDeclareId, @"processtep" : [self.dataArr[0] objectForKey:@"processtep"],@"shopreason" : @"noreason" ,@"shopaccessory" :@"/Picture/201409/041700468686.jpg"} mutableCopy];
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+    for (int i=0;i< [self.dataArr count];i++){
+        for (int j = 0;j< [[self.dataArr[i] objectForKey:@"ItemBody"] count];j++){
+            NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
+            [dic setObject:[self.dataArr[i] objectForKey:@"itemId"] forKey:@"itemid"];
+            [dic setObject:[[self.dataArr[i] objectForKey:@"ItemBody"][j] objectForKey:@"bodyname"] forKey:@"name"];
+            [dic setObject:[[self.dataArr[i] objectForKey:@"ItemBody"][j] objectForKey:@"bodytype"] forKey:@"type"];
+            [dic setObject:@"/Picture/201409/041700468686.jpg" forKey:@"img"];
+            [arr addObject:dic];
+        }
+    }
+    [sendDic setObject:arr forKey:@"ItemBody"];
+    NSString *sendJson = [sendDic JSONString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    request.URL = [NSURL URLWithString:[NSString createResponseURLWithMethod:@"set.acceptance.details" Params:sendJson]];
+    NSString *contentType = @"text/html";
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError){
+        NSString *retStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSString *retJson =[NSString decodeFromPercentEscapeString:[retStr decryptWithDES]];
+        NSDictionary *retDic = [retJson objectFromJSONString];
+        NSLog(@"%@",[retDic objectForKey:@"error"]);
+    }];
 }
 @end
