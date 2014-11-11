f14ceea10794ef4107b99a4fb15bffdb75485a63
hxx0215 <hxx0215@gmail.com>
2014-11-11 19:27:00 +0800

    综合 - 报建新增tableviewCell

---
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNNewReportTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  HNNewReportTableViewCell.h
+//  edecorate
+//
+//  Created by hxx on 11/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+typedef enum HNNewReportTableViewCellType{
+    HNNewReportTableViewCellTypeText,
+    HNNewReportTableViewCellTypeDate,
+    HNNewReportTableViewCellTypeButton,
+    HNNewReportTableViewCellTypeLabel
+}HNNewReportTableViewCellType;
+@interface HNNewReportTableViewCell : UITableViewCell
+
+@end
---
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNNewReportTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  HNNewReportTableViewCell.m
+//  edecorate
+//
+//  Created by hxx on 11/11/14.
+//
+//
+
+#import "HNNewReportTableViewCell.h"
+
+@implementation HNNewReportTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
