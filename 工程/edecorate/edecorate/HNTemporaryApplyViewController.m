4c3e11a670207bb5aaae19ad47b9e99496e4b88f
liuxh532 <786995209@qq.com>
2014-11-19 00:14:15 +0800

    上传图片修改

--- 工程/edecorate/edecorate/HNTemporaryApplyViewController.m
+++ 工程/edecorate/edecorate/HNTemporaryApplyViewController.m
@@ -14,6 +14,7 @@
 #import "HNLoginData.h"
 #import "HNDecorateChoiceView.h"
 #import "HNTemporaryApplyTableViewCell.h"
+#import "HNUploadImage.h"
 
 /*
  "House Information" = "房屋信息";
@@ -364,108 +365,25 @@
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     UIImage *image=[info objectForKey:UIImagePickerControllerOriginalImage];
     [self.imagePicker dismissViewControllerAnimated:YES completion:nil];
-    
-    self.imageUpload = image;
-    
-    CGFloat scaleSize = 0.5f;
-    UIGraphicsBeginImageContext(CGSizeMake(image.size.width * scaleSize, image.size.height * scaleSize));
-    [image drawInRect:CGRectMake(0, 0, image.size.width * scaleSize, image.size.height * scaleSize)];
-    UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
     MBProgressHUD *hud=[MBProgressHUD showHUDAddedTo:self.view animated:YES];
     hud.labelText = NSLocalizedString(@"Loading", nil);
-    [self requestForPostWithURLString:scaledImage];
+    [self requestForPostWithURLString:image];
 }
 
 -(void)requestForPostWithURLString:(UIImage*)image
 {
-    
-    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:@"testImage.png",@"name",nil];
-    NSString *jsonStr = [dic JSONString];
-    NSURL *URL = [NSURL URLWithString:[NSString createResponseURLWithMethod:@"set.picture.add" Params:jsonStr]];
-    
-    //    NSString *TWITTERFON_FORM_BOUNDARY = @"AaB03x";
-    NSString *TWITTERFON_FORM_BOUNDARY = @"AABBCC";
-    //根据url初始化request
-    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:URL cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10];
-    //分界线 --AaB03x
-    NSString *MPboundary=[[NSString alloc]initWithFormat:@"--%@",TWITTERFON_FORM_BOUNDARY];
-    //结束符 AaB03x--
-    NSString *endMPboundary=[[NSString alloc]initWithFormat:@"%@--",MPboundary];
-    //http body的字符串
-    NSMutableString *body=[[NSMutableString alloc]init];
-    
-    ////添加分界线，换行---文件要先声明
-    [body appendFormat:@"%@\r\n",MPboundary];
-    //声明pic字段，文件名为boris.png
-    [body appendFormat:@"Content-Disposition: form-data; name=\"imgFile\"; filename=\"testImage.JPEG\"\r\n"];
-    //声明上传文件的格式
-    [body appendFormat:@"Content-Type: image/JPEG\r\n\r\n"];
-    //声明结束符：--AaB03x--
-    NSString *end=[[NSString alloc]initWithFormat:@"\r\n%@",endMPboundary];
-    
-    //声明myRequestData，用来放入http body
-    NSMutableData *myRequestData=[NSMutableData data];
-    //将body字符串转化为UTF8格式的二进制
-    [myRequestData appendData:[body dataUsingEncoding:NSUTF8StringEncoding]];
-    //将image的data加入
-    //UIImage *images = [UIImage imageNamed:@"押金退款.png"];
-    //得到图片的data
-    NSData* imageData = UIImagePNGRepresentation(image);
-    [myRequestData appendData:imageData];
-    //加入结束符--AaB03x--
-    [myRequestData appendData:[end dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    //设置HTTPHeader中Content-Type的值
-    NSString *content=[[NSString alloc]initWithFormat:@"multipart/form-data; boundary=%@",TWITTERFON_FORM_BOUNDARY];
-    //设置HTTPHeader
-    [request setValue:content forHTTPHeaderField:@"Content-Type"];
-    //设置Content-Length
-    [request setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[myRequestData length]] forHTTPHeaderField:@"Content-Length"];
-    //设置http body
-    [request setHTTPBody:myRequestData];
-    //http method
-    [request setHTTPMethod:@"POST"];
-    //建立连接，设置代理
-    
-    
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError){
-        [self performSelectorOnMainThread:@selector(didUploadImage:) withObject:data waitUntilDone:YES];
+    [HNUploadImage UploadImage:image block:^(NSString *msg) {
+        [MBProgressHUD hideHUDForView:self.view animated:YES];
+        if (msg) {
+            self.imagePath = msg;
+            self.imageUpload = image;
+            [self.uploadButton setImage:image forState:UIControlStateNormal];
+        }
     }];
+     
     
 }
 
--(void)didUploadImage:(NSData *)data
-{
-    [MBProgressHUD hideHUDForView:self.view animated:YES];
-    if (data)
-    {
-        NSString *retStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        NSString *retJson =[NSString decodeFromPercentEscapeString:[retStr decryptWithDES]];
-        NSLog(@"%@",retJson);
-        NSDictionary* dic = [retJson objectFromJSONString];
-        //    {"verification":true,"total":1,"data":[{"state":1,"msg":"http://113.105.159.115/Picture/201410/302117447717.png"}],"error":null}
-        if ([[dic objectForKey:@"total"] integerValue]>=1)
-        {
-            NSArray* array = [dic objectForKey:@"data"];
-            NSDictionary *dicData = [array objectAtIndex:0];
-            self.imagePath = [dicData objectForKey:@"msg"];
-            
-            [self.uploadButton setImage:self.imageUpload forState:UIControlStateNormal];
-        }
-        else
-        {
-            self.imageUpload = nil;
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Loading Fail", nil) message:NSLocalizedString(@"Please try again", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles: nil];
-            [alert show];
-        }
-    }
-    else{
-        self.imageUpload = nil;
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Connection Error", nil) message:NSLocalizedString(@"Please check your network.", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles: nil];
-        [alert show];
-    }
-}
 
 
 //-(void)viewWillAppear:(BOOL)animated
