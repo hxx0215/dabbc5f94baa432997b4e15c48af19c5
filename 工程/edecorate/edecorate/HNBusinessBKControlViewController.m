8075b7540606243111d44bd18c0574692dfda2b7
liuxh532 <786995209@qq.com>
2014-11-20 23:51:22 +0800

    商家后台管理主页面

--- 工程/edecorate/edecorate/HNBusinessBKControlViewController.m
+++ 工程/edecorate/edecorate/HNBusinessBKControlViewController.m
@@ -31,11 +31,13 @@
 
 @end
 
-#define HSPACE 50
-#define WSPACE 15
+#define HSPACE 126/2
+#define WLEFT 30/2
+#define WRIGHT 30/2
+#define WSPACE 44/2
 #define TSPACEPER 0.1
-#define BTNHEIGHT 50
-#define STARTTOP 50
+#define STARTTOP 266/2
+#define BTNWIDTH 100
 
 @implementation HNBusinessBKControlViewController
 
@@ -43,20 +45,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self messageButtonWithTitle:NSLocalizedString(@"Pending payment", nil) button:self.messagePayingButton numMessage:3 index:0];
-    [self messageButtonWithTitle:NSLocalizedString(@"To be shipped", nil) button:self.messageShippingButton numMessage:3 index:1];
-    [self messageButtonWithTitle:NSLocalizedString(@"Shipped", nil) button:self.messageShippedButton numMessage:3 index:2];
-    [self messageButtonWithTitle:NSLocalizedString(@"Successful transaction", nil) button:self.messageSuccessfulButton numMessage:3 index:3];
     
-    [self businessButtonWithTitle:NSLocalizedString(@"My merchandise", nil) button:self.merchandiseButton rowIndex:0 coloumIndex:0];
-    [self businessButtonWithTitle:NSLocalizedString(@"Order Management", nil) button:self.orderButton rowIndex:0 coloumIndex:1];
-    [self businessButtonWithTitle:NSLocalizedString(@"Returns Management", nil) button:self.returnsButton rowIndex:0 coloumIndex:2];
-    [self businessButtonWithTitle:NSLocalizedString(@"Comments", nil) button:self.commentsButton rowIndex:1 coloumIndex:0];
-    [self businessButtonWithTitle:NSLocalizedString(@"My Profile", nil) button:self.profileButton rowIndex:1 coloumIndex:1];
-    [self businessButtonWithTitle:NSLocalizedString(@"Consumption volume verification", nil) button:self.volumeButton rowIndex:1 coloumIndex:2];
+//    [self messageButtonWithTitle:NSLocalizedString(@"Pending payment", nil) button:self.messagePayingButton numMessage:3 index:0];
+//    [self messageButtonWithTitle:NSLocalizedString(@"To be shipped", nil) button:self.messageShippingButton numMessage:3 index:1];
+//    [self messageButtonWithTitle:NSLocalizedString(@"Shipped", nil) button:self.messageShippedButton numMessage:3 index:2];
+//    [self messageButtonWithTitle:NSLocalizedString(@"Successful transaction", nil) button:self.messageSuccessfulButton numMessage:3 index:3];
     
     self.navigationItem.title = NSLocalizedString(@"Business Background", nil);
-    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor colorWithRed:230/255.0 green:230/255.0 blue:230/255.0 alpha:1];
+    
+    self.merchandiseButton = [self createButtonWithTitle:NSLocalizedString(@"My merchandise", nil) selector:@selector(merchandiseClicked:) row:0 coloum:0 image:[UIImage imageNamed:@"我的商品"] imageClick:[UIImage imageNamed:@"我的商品c"]];
+    
+    self.orderButton = [self createButtonWithTitle:NSLocalizedString(@"Order Management", nil) selector:@selector(orderClicked:) row:0 coloum:1 image:[UIImage imageNamed:@"订单管理"] imageClick:[UIImage imageNamed:@"订单管理c"]];
+    self.returnsButton = [self createButtonWithTitle:NSLocalizedString(@"Returns Management", nil) selector:@selector(returnsClicked:) row:0 coloum:2 image:[UIImage imageNamed:@"退换货管理"] imageClick:[UIImage imageNamed:@"退换货管理c"]];
+    self.commentsButton = [self createButtonWithTitle:NSLocalizedString(@"Comments", nil) selector:@selector(commentsClicked:) row:1 coloum:0 image:[UIImage imageNamed:@"评论留言"] imageClick:[UIImage imageNamed:@"评论留言c"]];
+    
+    self.profileButton = [self createButtonWithTitle:NSLocalizedString(@"My Profile", nil) selector:@selector(profileClicked:) row:1 coloum:1 image:[UIImage imageNamed:@"我的资料"] imageClick:[UIImage imageNamed:@"我的资料c"]];
+    self.volumeButton = [self createButtonWithTitle:NSLocalizedString(@"Consumption volume verification", nil) selector:@selector(volumeClicked:) row:1 coloum:2 image:[UIImage imageNamed:@"消费卷验证"] imageClick:[UIImage imageNamed:@"消费卷验证c"]];
+}
+
+- (UIButton *)createButtonWithTitle:(NSString *)title selector:(SEL)selector row:(int)ro coloum:(int)col image:(UIImage* )image imageClick:(UIImage* )imageClick{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor whiteColor];
+    btn.width = (self.view.width-20)/3;
+    btn.height = (1.0*btn.width/image.size.width)*image.size.height;
+    //    [btn setImage:image forState:UIControlStateNormal];
+    //    [btn setImage:imageClick forState:UIControlStateSelected];
+    [btn setBackgroundImage:image forState:UIControlStateNormal];
+    [btn setBackgroundImage:imageClick forState:UIControlStateHighlighted];
+    //［btn set
+    //[btn.titleLabel setContentMode:UIViewContentModeBottom];
+    //[btn sizeToFit];
+    CGFloat f = 8;
+    btn.left = f+(2+btn.width)*col;
+    btn.top = f+(2+btn.height)*ro;
+    
+    [btn setTitle:title forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor darkTextColor] forState:UIControlStateNormal];
+    btn.titleEdgeInsets = UIEdgeInsetsMake(btn.height-24, 0, 0, 0);
+    UILabel* label = btn.titleLabel;
+    //    label.text = title;
+    //    label.textAlignment = NSTextAlignmentCenter;
+    //    label.textColor = [UIColor blackColor];
+    label.font = [UIFont systemFontOfSize:12];
+    [btn bringSubviewToFront:label];
+    
+    //btn.layer.borderWidth = 1.0;
+    //btn.layer.borderColor = [UIColor blackColor].CGColor;
+    [btn addTarget:self action:selector forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    return btn;
 }
 
 - (UIButton *)messageButtonWithTitle:(NSString *)title button:(UIButton*)btn numMessage:(NSInteger)num index:(NSInteger)colom
@@ -67,22 +105,6 @@
     btn.width = self.view.width/4;
     btn.height = 50;
     btn.left = self.view.width/4*colom;
-    btn.layer.borderWidth = 1.0;
-    btn.layer.borderColor = [UIColor blackColor].CGColor;
-    btn.titleLabel.textAlignment = NSTextAlignmentLeft;
-    btn.titleLabel.numberOfLines = 2;
-    //btn.font = [UIFont systemFontOfSize:12];
-    return btn;
-}
-
-- (UIButton *)businessButtonWithTitle:(NSString *)title button:(UIButton*)btn rowIndex:(NSInteger)row coloumIndex:(NSInteger)coloum
-{
-    [btn setTitle:title forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    btn.width = (self.view.width - 5 * WSPACE)/3;
-    btn.height = BTNHEIGHT;
-    btn.left = WSPACE+(btn.width+WSPACE)*coloum;
-    btn.top = 50+HSPACE+(BTNHEIGHT+HSPACE)*row;
     btn.layer.borderWidth = 1.0;
     btn.layer.borderColor = [UIColor blackColor].CGColor;
     btn.titleLabel.textAlignment = NSTextAlignmentLeft;
