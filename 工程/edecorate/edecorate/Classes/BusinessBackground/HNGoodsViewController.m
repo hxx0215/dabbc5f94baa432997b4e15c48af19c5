51a76c9dfe879c420ade0a2c2a64d9bef7f6d860
hxx0215 <hxx0215@gmail.com>
2014-11-25 20:02:12 +0800

    我的商品 详情

--- 工程/edecorate/edecorate/Classes/BusinessBackground/HNGoodsViewController.m
+++ 工程/edecorate/edecorate/Classes/BusinessBackground/HNGoodsViewController.m
@@ -8,10 +8,28 @@
 
 #import "HNGoodsViewController.h"
 #import "HNLoginData.h"
+#import "HNImageData.h"
 
 @interface HNGoodsViewController ()
 @property (strong, nonatomic) IBOutlet UIScrollView *backView;
 @property (strong, nonatomic) NSMutableDictionary *goodsDetail;
+@property (strong, nonatomic) IBOutlet UILabel *goodsname;
+@property (strong, nonatomic) IBOutlet UILabel *groupprice;
+@property (strong, nonatomic) IBOutlet UILabel *stock;
+@property (strong, nonatomic) IBOutlet UILabel *goodstype;
+@property (strong, nonatomic) IBOutlet UILabel *goodsitem;
+@property (strong, nonatomic) IBOutlet UILabel *returnintegral;
+@property (strong, nonatomic) IBOutlet UILabel *isfreedelivery;
+@property (strong, nonatomic) IBOutlet UILabel *weight;
+@property (strong, nonatomic) IBOutlet UILabel *maxbought;
+@property (strong, nonatomic) IBOutlet UILabel *virtualcount;
+@property (strong, nonatomic) IBOutlet UILabel *begintime;
+@property (strong, nonatomic) IBOutlet UILabel *endtime;
+@property (strong, nonatomic) IBOutlet UILabel *classid1;
+@property (strong, nonatomic) IBOutlet UILabel *classid2;
+@property (strong, nonatomic) IBOutlet UILabel *classid3;
+@property (strong, nonatomic) IBOutlet UILabel *intro;
+@property (strong, nonatomic) IBOutlet UIImageView *img;
 
 
 @end
@@ -33,7 +51,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)showData{
+    self.img.image = [[HNImageData shared] imageWithLink:self.goodsDetail[@"imgurl"]];
+    self.goodsname.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"goodsname"]];
+    self.groupprice.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"groupprice"]];
+    self.stock.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"stock"]];
+    self.goodstype.text = [[NSString stringWithFormat:@"%@",self.goodsDetail[@"goodstype"]] isEqualToString:@"1"] ? NSLocalizedString(@"实体商品", nil) : NSLocalizedString(@"虚拟商品", nil);
+    self.goodsitem.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"goodsitem"]];
+    self.returnintegral.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"returnintegral"]];
+    self.isfreedelivery.text = [[NSString stringWithFormat:@"%@",self.goodsDetail[@"isfreedelivery"]] isEqualToString:@"1"] ? NSLocalizedString(@"是", nil) : NSLocalizedString(@"否", nil);
+    self.weight.text = [NSString stringWithFormat:@"%@%@",self.goodsDetail[@"weight"],self.goodsDetail[@"weightUnit"]];
+    self.maxbought.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"maxbought"]];
+    self.virtualcount.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"virtualcount"]];
+    self.begintime.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"begintime"]];
+    self.endtime.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"endtime"]];
+    self.classid1.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"classid1"]];
+    self.classid2.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"classid2"]];
+    self.classid3.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"classid3"]];
+    self.intro.text = [NSString stringWithFormat:@"%@",self.goodsDetail[@"intro"]];
+}
 - (void)loadData{
     NSDictionary *sendDic = @{@"mshopid": [HNLoginData shared].mshopid,@"goodsid" : self.goodsid,@"imgwidth" : @"89", @"imgheight" : @"89"};
     NSString *sendJson = [sendDic JSONString];
@@ -49,7 +85,8 @@
             NSDictionary *retDic = [retJson objectFromJSONString];
             NSInteger count = [[retDic objectForKey:@"total"] integerValue];
             if (0!=count){
-
+                self.goodsDetail = [[retDic objectForKey:@"data"][0] mutableCopy];
+                [self showData];
             }
             else
                 [self showNoData];
