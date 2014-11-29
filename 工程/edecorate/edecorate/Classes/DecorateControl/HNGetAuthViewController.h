49e3fb379c183f4f641402d4a8e1dcd310b1fa98
hxx0215 <hxx0215@gmail.com>
2014-11-29 22:16:21 +0800

    开通商家认证

---
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNGetAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNGetAuthViewController.h
+//  edecorate
+//
+//  Created by hxx on 11/29/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNGetAuthViewController : UIViewController
+
+@end
---
+++ 工程/edecorate/edecorate/Classes/DecorateControl/HNGetAuthViewController.m
@@ -0,0 +1,37 @@
+//
+//  HNGetAuthViewController.m
+//  edecorate
+//
+//  Created by hxx on 11/29/14.
+//
+//
+
+#import "HNGetAuthViewController.h"
+
+@interface HNGetAuthViewController ()
+
+@end
+
+@implementation HNGetAuthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
