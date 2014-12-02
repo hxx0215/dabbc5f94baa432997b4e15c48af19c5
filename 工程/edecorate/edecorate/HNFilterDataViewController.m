b5075137b3f401598ded68565dcf4f535195b9e9
hxx0215 <hxx0215@gmail.com>
2014-12-02 09:13:16 +0800

    增加corelocation位置以及我的商品筛选定位

--- 工程/edecorate/edecorate/HNFilterDataViewController.m
+++ 工程/edecorate/edecorate/HNFilterDataViewController.m
@@ -8,9 +8,10 @@
 
 #import "HNFilterDataViewController.h"
 #import "HNCityChoiceViewController.h"
-
-@interface HNFilterDataViewController ()
-
+#import <CoreLocation/CoreLocation.h>
+@interface HNFilterDataViewController ()<CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet UIButton *locationName;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 @end
 
 @implementation HNFilterDataViewController
@@ -18,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.locationManager = [CLLocationManager new];
+    self.locationManager.delegate = self;
 }
 
 -(IBAction)cityChoiceClick:(id)sender
@@ -30,7 +33,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.locationManager startUpdatingLocation];
+}
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *array, NSError *error)
+     {
+         if (array.count > 0)
+         {
+             CLPlacemark *placemark = [array objectAtIndex:0];
+             
+             //将获得的所有信息显示到label上
+//             self.location.text = placemark.name;
+             //获取城市
+             NSString *city = placemark.locality;
+             if (!city) {
+                 //四大直辖市的城市信息无法通过locality获得，只能通过获取省份的方法来获得（如果city为空，则可知为直辖市）
+                 city = placemark.administrativeArea;
+             }
+             [self.locationName setTitle:city forState:UIControlStateNormal];
+             
+         }
+         else if (error == nil && [array count] == 0)
+         {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 [self.locationName setTitle:NSLocalizedString(@"无法定位", nil) forState:UIControlStateNormal];
+                 UIAlertView *alert =[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"警告", nil) message:NSLocalizedString(@"无法获得您当前的位置请手动选择城市", nil) delegate:nil cancelButtonTitle:NSLocalizedString(@"确定", nil) otherButtonTitles: nil];
+                 [alert show];
+             });
+         }
+         else if (error != nil)
+         {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 [self.locationName setTitle:NSLocalizedString(@"无法定位", nil) forState:UIControlStateNormal];
+             });
+         }
+     }];
 
+}
 /*
 #pragma mark - Navigation
 
